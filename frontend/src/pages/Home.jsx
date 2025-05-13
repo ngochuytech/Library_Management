c40cc827c646e05903b2c0fe2cc03de1e164a421
Nguyen Van Ngoc Huy <ngochuytech23@gmail.com>
2025-05-13 14:14:57 +0700

    fix2

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -47,13 +47,13 @@ const HomePage = () => {
   const [totalPages, setTotalPages] = useState();
 
   const [recommendedBooks, setRecommendedBooks] = useState([]);
+  const [recentlyBooks, setRecentlyBooks] = useState([]);
 
   const BASE_URL = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     const fetchRecommendedBooks = async () => {
       try {
-
         const response = await fetch(`${BASE_URL}/books/api`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -61,7 +61,6 @@ const HomePage = () => {
         const data = await response.json();
 
         setRecommendedBooks(data.results);
-
       } catch (error) {
         console.error("Failed to fetch recommended books:", error);
       }
@@ -70,7 +69,6 @@ const HomePage = () => {
     const fetchRecentlyBooks = async () => {
       setRecentlyBooks([]);
       try {
-
         const response = await fetch(`${BASE_URL}/books/api?page=2`);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
@@ -78,7 +76,6 @@ const HomePage = () => {
         const data = await response.json();
 
         setRecentlyBooks(data.results);
-
       } catch (error) {
         console.error("Failed to fetch recently books:", error);
       }
@@ -92,7 +89,7 @@ const HomePage = () => {
       fetchAllBook(currentPage);
     else
       fetchSearchResults(currentPage);
-  });
+  }, [activeView, searchQuery, searchType, currentPage]);
   const fetchSearchResults = async (page) => {
     try {
       const response = await fetch(`${BASE_URL}/books/api?type=${searchType}&query=${searchQuery}&page=${page}`);
@@ -195,7 +192,7 @@ const HomePage = () => {
           />
         );
       case "bookshelf":
-        return <MyBookshelf books={recommendedBooks} />;
+        return <MyBookshelf />;
       case "contributions":
         return <Contributions />;
       case "RecommendBooks":
@@ -319,7 +316,7 @@ const HomePage = () => {
 
                   <div className="user-profile" onClick={() => setShowUserMenu(!showUserMenu)}>
                     <div className="avatar">VT</div>
-                    <span>{sessionStorage.getItem("user") === null ? "Nguyễn Văn A" : sessionStorage.getItem("username")}</span>
+                    <span>{sessionStorage.getItem("username") === null ? "Nguyễn Văn A" : sessionStorage.getItem("username")}</span>
                     <span className="dropdown-icon">▼</span>
                     {showUserMenu && (
                       <div className="user-dropdown">
