07a45c0a6921231b93ebfac9db30632bf0c73fc3
ExTric6224 <doanminh06022004@gmail.com>
2025-05-24 00:25:40 +0700

    add quantity == available

--- frontend/src/components/AdminBooks.jsx
+++ frontend/src/components/AdminBooks.jsx
@@ -66,10 +66,12 @@ const AdminBooks = () => {
     setLoading(true);
     setError(null); // Reset lỗi trước mỗi lần fetch
     try {
-      const url =  `${BASE_URL}/books/api/search?type=title&query=${encodeURIComponent(search)}&page=${page}`;
+      const url = `${BASE_URL}/books/api/search?type=title&query=${encodeURIComponent(
+        search
+      )}&page=${page}`;
 
       const response = await api.get(url);
-
+      console.log("Response data:", response.data); // Kiểm tra dữ liệu trả về
       if (response.data && response.data.results) {
         setBooks(response.data.results);
         const totalItems = response.data.count || 0;
@@ -206,7 +208,8 @@ const AdminBooks = () => {
   };
 
   const handleDeleteBook = async () => {
-    if (bookToDelete) {
+    console.log("Deleting book:", bookToDelete);
+    if (bookToDelete && bookToDelete.quantity == bookToDelete.available) {
       try {
         const token = sessionStorage.getItem(ACCESS_TOKEN);
         const requestHeaders = {
@@ -237,6 +240,8 @@ const AdminBooks = () => {
         alert("Đã xảy ra lỗi khi xóa sách");
         setShowDeleteConfirm(false); // Đảm bảo modal đóng khi có lỗi
       }
+    } else {
+      alert("Không thể xóa sách này vì đang có người mượn.");
     }
   };
 
